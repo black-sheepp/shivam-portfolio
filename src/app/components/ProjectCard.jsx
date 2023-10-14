b21797780400eb47cce877db55c396abbb8766d4
Shivam <shivamguptanitw@gmail.com>
2023-10-15 00:19:06 +0530

    Added responsiveness to Project Section UI

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -16,9 +16,9 @@ const ProjectCard = ({ projectName, description, technologies, gitHubRepository,
 	// }, []);
 	return (
 		<Link href={gitHubRepository} target='_blank'>
-			<div className=' rounded-2xl border-solid border-2 border-white overflow-hidden shadow-lg cursor-pointer'>
+			<div className='w-11/12 rounded-2xl border-solid border-2 border-white overflow-hidden shadow-lg cursor-pointer'>
 				<Image
-					className='w-full object-cover h-[220px] rounded-2xl'
+					className='w-full object-cover h-[220px]'
 					src={thumbnail}
 					alt='Sunset in the mountains'
 					width={"400"}
@@ -26,7 +26,7 @@ const ProjectCard = ({ projectName, description, technologies, gitHubRepository,
 				/>
 				<div className='px-6 py-2'>
 					<div className='text-xl mb-2 text-pink'>{projectName}</div>
-					<p className='text-grey text-base'>{description}</p>
+					<p className='text-grey text-base hidden lg:flex'>{description}</p>
 				</div>
 				<div className='px-6 pt-1 pb-2'>
 					{technologies.map((tech, index) => (
